d86912041b6986f90527db6580c62c9f97147098
D066867 <denis.holdeew@sap.com>
2021-05-28 10:58:47 +0200

    Add UI5 types

--- Helper.ts
+++ Helper.ts
@@ -1,5 +1,13 @@
+import Label from "sap/m/Label";
+import ODataModel from "sap/ui/model/odata/v4/ODataModel";
 export class Helper {
     someMethod() {
+        let label = new Label();
+        let oDataModel = new ODataModel({serviceUrl: "someUrl", synchronizationMode: "None"});
+        oDataModel.bindList("somePath").create().created().then(() => {
+            //Some entity was created
+        })
+        label.getText();
         return true;
     }
 }
